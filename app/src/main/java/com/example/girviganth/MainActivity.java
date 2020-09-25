3ec6eb35b43592f137bace172da67981bb20809d
Nitin Agarwal <nitinagrawal7@gmail.com>
2020-09-25 17:50:23 +0530

    Search Function Try

--- app/src/main/java/com/example/girviganth/MainActivity.java
+++ app/src/main/java/com/example/girviganth/MainActivity.java
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.TargetApi;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -51,8 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
         spinner_branch.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position,
-                                       long id) {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 // On selecting a spinner item
                 String label = parent.getItemAtPosition(position).toString();
                 selectedVal =Integer.parseInt( branch_id.get(position));
@@ -61,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
                 storeDataInArrays(selectedVal);
                 //for re-fresh Activity
-                //customAdapter = new CustomAdapter(MainActivity.this, customer_id, customer_name, customer_father, customer_village, customer_phone);
                 customerAdapter = new CustomerAdapter(MainActivity.this,parent.getContext(), customer_id, customer_name, customer_father, customer_village, customer_phone);
                 recyclerView.setAdapter(customerAdapter);
                 recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
@@ -79,10 +84,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                                           }
         } );
-
-
-
-
     }
     //for re-fresh Activity
     @Override
@@ -111,17 +112,10 @@ public class MainActivity extends AppCompatActivity {
                 customer_father.add(cursor.getString(3));
                 customer_village.add(cursor.getString(4));
                 customer_phone.add(cursor.getString(5));
-
             }
         }
+    }
 
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.my_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
     private void loadBranchSpinner(){
         List<String> list = new ArrayList<String>();
         Cursor cursor = myDB.readAllBranch();
@@ -134,19 +128,49 @@ public class MainActivity extends AppCompatActivity {
                 branch_id.add(cursor.getString(0));
                 branch_name.add(cursor.getString(1));
             }
-            /*if (cursor.moveToFirst()) {
-                do {
-                    list.add(cursor.getString(1));//adding 2nd column data
-                } while (cursor.moveToNext());
-            }*/
         }
         // Creating adapter for spinner
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, branch_name);
-
         // Drop down layout style - list view with radio button
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         // attaching data adapter to spinner
         spinner_branch.setAdapter(dataAdapter);
+    }
+
+    @Override
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+        SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
+        searchView.setQueryHint("Search Customer");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Log.d(TAG, "onQueryTextSubmit ");
+                cursor=studentRepo.getStudentListByKeyword(s);
+                if (cursor==null){
+                    Toast.makeText(MainActivity.this,"No records found!",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this, cursor.getCount() + " records found!",Toast.LENGTH_LONG).show();
+                }
+                customerAdapter.swapCursor(cursor);
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s) {
+                Log.d(TAG, "onQueryTextChange ");
+                cursor=studentRepo.getStudentListByKeyword(s);
+                if (cursor!=null){
+                    customAdapter.swapCursor(cursor);
+                }
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
